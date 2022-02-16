08475ad8c5259b4d71ad8892646df5dc01d518d8
moringa <tressy.mbone@student.moringaschool.com>
2022-02-16 16:27:15 +0300

    deploy to heroku

--- app/requests.py
+++ app/requests.py
@@ -1,5 +1,10 @@
-import requests
+from urllib import response
+import urllib.request,json
 def get_quote():
-    url="http://quotes.stormconsultancy.co.uk/random.json".format()
-    main_url=requests.get(url).json()
-    return  main_url
+    
+    urlget="http://quotes.stormconsultancy.co.uk/random.json".format()
+    with urllib.request.urlopen(urlget) as url:
+        quote=url.read()
+        response=json.loads(quote)
+        return response
+   
